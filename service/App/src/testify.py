4dc767cd82fcd7f6bb3c1467451ce39ff9f570cc
Clemens <c.lottermoser@campus.tu-berlin.de>
2021-05-05 21:37:08 +0200

    first POST API for appointment

--- service/App/src/testify.py
+++ service/App/src/testify.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, json, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/make_appointment', methods=['POST'])
+def make_appointment():
+    print(request.form['prename'])
+    print(request.form['lastname'])
+    print(request.form['date'])
+    return 'ok', 200
 
 
 if __name__ == '__main__':
