07a9d21834df58956fe9f67688861cd71284819b
Clemens <c.lottermoser@campus.tu-berlin.de>
2021-07-05 21:55:35 +0200

    excluded doctors from online_users list

--- service/App/src/online_users.py
+++ service/App/src/online_users.py
@@ -4,8 +4,8 @@ import subprocess
 def get_online_users():
     subprocess.Popen(
         'mysqldump -h testify-mysql -u usertable_user user_database --no-tablespaces --lock-tables=false '
-        '--no-create-info --net-buffer-length=1048576 --extended-insert=FALSE --compact --hex-blob --where="1 order '
-        'by user_id DESC limit 1000" > user_data/online_users/dump.sql',
+        '--no-create-info --net-buffer-length=1048576 --extended-insert=FALSE --compact --hex-blob --where="1 and '
+        'username not like \'doctor%\' order by user_id DESC limit 1000" > user_data/online_users/dump.sql',
         shell=True).wait()
     try:
         f = open("user_data/online_users/dump.sql", "r")
