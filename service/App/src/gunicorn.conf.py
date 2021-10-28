92de7087298a1919f6fd21ef5857b18c5dd5cabb
Louis Burda <quent.burda@gmail.com>
2021-10-29 01:29:32 +0200

    tweak gunicorn confs for github runner timeout

--- service/App/src/gunicorn.conf.py
+++ service/App/src/gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 worker_class = "eventlet"
-workers = multiprocessing.cpu_count()*2+1
-keepalive = 10
-
+workers = multiprocessing.cpu_count() * 2 + 1
 bind = "0.0.0.0:8000"
+keepalive = 60
+timeout = 60
